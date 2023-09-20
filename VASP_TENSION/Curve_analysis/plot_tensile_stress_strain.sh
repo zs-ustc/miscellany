3fcb0fc6fcd2b38a976067f97429b3807e4bc487
zs-ustc <shuaizhao@mail.ustc.edu.cn>
2023-09-21 02:01:29 +0800

    Update plot_tensile_stress_strain.sh

--- VASP_TENSION/Curve_analysis/plot_tensile_stress_strain.sh
+++ VASP_TENSION/Curve_analysis/plot_tensile_stress_strain.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+workspace=tension
 
-paste strainx.dat stressx.dat > strain_stress_x.dat
+
+paste ../${workspace}/strainx.dat ../${workspace}/stressx.dat > strain_stress_x.dat
 python plot_tensile_stress_strain.py strain_stress_x.dat --title 'tensile direction: x' --Yn 3 -t y --I2D 1 --label 'DFT-x:fitting'
 
 
-paste strainy.dat stressy.dat > strain_stress_y.dat
+paste ../${workspace}/strainy.dat ../${workspace}/stressy.dat > strain_stress_y.dat
 python plot_tensile_stress_strain.py strain_stress_y.dat --title 'tensile direction: y' --Yn 3 -t y --I2D 1 --label 'DFT-y:fitting'
 
