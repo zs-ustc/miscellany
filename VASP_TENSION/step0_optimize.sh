1b2a2bc08649dffdf2ed0871ebf9ff43e9408a07
zs-ustc <shuaizhao@mail.ustc.edu.cn>
2023-09-21 14:02:26 +0800

    Update step0_optimize.sh

--- VASP_TENSION/step0_optimize.sh
+++ VASP_TENSION/step0_optimize.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Files used in data directory: POSCAR_filename, INCAR.isif3, vasp_relax.sub
+# Files used in data directory: POSCAR_dir/POSCAR_filename, INCAR.isif3, vasp_relax.sub
 
 # Parameters setting
-	I2D=1
+    I2D=1
     NCORE=16
     # filename is the filename of POSCAR stored in data
+    POSCAR_dir=.
     filename=POSCAR
     workspace=tension
     # Batch type
@@ -19,12 +20,12 @@ Workspace    :   ${workspace}
 Batch type   :   ${batch_type}" 
 # Relaxation calculation
 pwd_init=`pwd`
-if [ -f data/${filename} ];then
+if [ -f data/${POSCAR_dir}/${filename} ];then
     # Make directory
     mkdir -p ${pwd_init}/${workspace}/0.relax
 
     # POSCAR and POTCAR for all calculations
-    cp ${pwd_init}/data/${filename} ${pwd_init}/${workspace}/POSCAR
+    cp ${pwd_init}/data/${POSCAR_dir}/${filename} ${pwd_init}/${workspace}/POSCAR
 	cd ${pwd_init}/${workspace}
 	[[ ${I2D}=1 ]] && echo -e "923\n1\n"|vaspkit|grep POSCAR_REV && mv -f POSCAR_REV POSCAR
     echo -e "103\n"|vaspkit | grep "POTCAR File No."
@@ -39,14 +40,14 @@ if [ -f data/${filename} ];then
 	
     # Submitting script
     cp ${pwd_init}/data/vasp.sub ${filename}_relax.sub
-	echo "mv ${pwd_init}/data/${filename} ${pwd_init}/data/${filename}.init">>${filename}_relax.sub
+	#echo "mv ${pwd_init}/data/${filename} ${pwd_init}/data/${filename}.init">>${filename}_relax.sub
 	echo "cp CONTCAR ${pwd_init}/data/${filename}_relaxed">>${filename}_relax.sub
 
     # Submit jobs
     if [ ${batch_type} == sh ];then
         nohup sh *.sub > ${workspace}_relax.out 2>&1 &
     else
-        ${fp_cmd} *.sub
+        ${batch_type} *.sub
     fi
 
     # Get back to initial directory
